9380ce7cef7a975db0298d82e241fd9633df8dd2
Johanna Simfors <jsimfors@gmail.com>
2020-12-16 22:43:05 +0100

    Working basis for all case 1 tests

--- src/main/kotlin/furhatos/app/fruitseller/flow/usertest.kt
+++ src/main/kotlin/furhatos/app/fruitseller/flow/usertest.kt
@@ -61,7 +61,7 @@ val StartTest = state(InteractionTest){
     onEntry {
         //furhat.ask("Are you ready for the user test?") // Kommentera tillbaka
         //goto(C)
-        goto(A1) // ta bort
+        goto(A6) // ta bort
     }
 
     onResponse<Yes> {
@@ -143,7 +143,9 @@ val A1 = state(InteractionTest){
 
     // 1a. More facial gestures, a lot of gazing and glancing
     onEntry {
+        furhat.say("A1")
         furhat.gesture(Gestures.Surprise(strength = 4.00, duration=6.00), async = true)
+        furhat.say("Hey I am so glad to see you detective!")
         furhat.gesture(Gestures.Blink(duration=0.5), async=true)
         furhat.gesture(quickRightGlance)
         furhat.gesture(Gestures.Blink(duration=0.5), async=true)
@@ -153,7 +155,9 @@ val A1 = state(InteractionTest){
         furhat.gesture(Gestures.Blink(duration=0.1), async=true)
         furhat.gesture(Gestures.Blink(duration=0.1), async=true)
         furhat.gesture(quickRightGlance)
-        furhat.say(" I was at home. I went to bed around 11. ")
+        furhat.say(" I was at home.")
+        furhat.gesture(Gestures.Blink(duration=0.4), async=true)
+        furhat.say("I went to bed around 11. ")
         furhat.gesture(Gestures.GazeAway, async = true)
         furhat.say(" I was a close friend of Albert, I would never hurt him.")
         furhat.gesture(quickRightGlance)
@@ -172,8 +176,6 @@ val A1 = state(InteractionTest){
 val A2 = state(InteractionTest){
     // 1b Non-genuine smiles
 
-
-
     val nonHappyEyes = defineGesture {
         // Used with the smile-gesture to resemble non-genuine smiles
         frame(2.22, 6.44) {
@@ -189,10 +191,11 @@ val A2 = state(InteractionTest){
 
 
     onEntry {
-        furhat.gesture(Gestures.Smile(strength = 2.00), async = true)
-        furhat.gesture(Gestures.BrowFrown)
-        furhat.gesture(nonHappyEyes)
+        furhat.say("A2")
 
+        furhat.gesture(Gestures.BigSmile(strength = 2.0, duration=3.00), async = true)
+        furhat.gesture(nonHappyEyes)
+        furhat.gesture(Gestures.BrowFrown)
         furhat.say("Hey I am so glad to see you detective!")
         furhat.gesture(Gestures.Smile(strength = 1.00, duration = 4.0), async = true)
         furhat.gesture(Gestures.BrowFrown)
@@ -201,7 +204,7 @@ val A2 = state(InteractionTest){
         furhat.gesture(Gestures.BigSmile(duration = 5.0), async = true)
         furhat.gesture(nonHappyEyes)
         furhat.say("Hahaha I mean at home, I was at home. I was a close friend of Albert, I would never hurt him.")
-        furhat.gesture(Gestures.Smile(strength = 2.0, duration=3.00), async = true)
+        furhat.gesture(Gestures.BigSmile(strength = 2.0, duration=3.00), async = true)
         furhat.gesture(nonHappyEyes)
         furhat.gesture(Gestures.BrowFrown)
         furhat.say("Trust me")
@@ -217,23 +220,28 @@ val A2 = state(InteractionTest){
 val A3 = state(InteractionTest){
     // 1c Total lack of smile
     val halfClosedEyes = defineGesture {
-        frame(0.22, 3.44) {
-            BasicParams.BLINK_RIGHT to 0.2
-            BasicParams.BLINK_LEFT to 0.2
+        frame(0.22, 5.44) {
+            BasicParams.BLINK_RIGHT to 0.25
+            BasicParams.BLINK_LEFT to 0.25
         }
-        frame(3.50, 3.60) {
+        frame(5.50, 5.60) {
             BasicParams.BLINK_RIGHT to 0.0
             BasicParams.BLINK_LEFT to 0.0
 
         }
     }
 
-
     onEntry {
-        furhat.gesture(halfClosedEyes)
-        furhat.say("Hey I am so glad to see you detective! You are asking what I did at the night of the murder? I was at home. " +
-                "I went to bed around 11" +
-                "I was a close friend of Albert, I would never hurt him. Trust me.")
+        furhat.say("A3")
+
+        furhat.gesture(halfClosedEyes, async = true)
+        furhat.say("Hey I am so glad to see you detective!")
+        furhat.gesture(halfClosedEyes, async = true)
+        furhat.say("You are asking what I did at the night of the murder? I was at home. ")
+        furhat.gesture(halfClosedEyes, async = true)
+        furhat.say("I went to bed around 11" + "I was a close friend of Albert")
+        furhat.gesture(halfClosedEyes, async = true)
+        furhat.say("I would never hurt him. Trust me.")
         delay(4000) // TODO: Change to listens for "yes" bf continuing
         goto(A4)
     }
@@ -247,22 +255,31 @@ val A4 = state(InteractionTest){
     // 1d Displays a feeling of contempt.
 
     val rollEyes = defineGesture {
-        frame(0.22, 0.44) {
-            BasicParams.LOOK_UP to 1.0
-        }
         frame(0.50, 0.60) {
+            BasicParams.LOOK_UP to 0.5
+        }
+        frame(0.80, 0.90) {
             BasicParams.LOOK_UP to 0.0
         }
     }
 
+    val resetFace = defineGesture {
+        reset(0.00)
+    }
+
     onEntry {
-        furhat.gesture(Gestures.ExpressDisgust, async = true)
-        furhat.gesture(Gestures.BrowFrown(strength = 2.00))
-        furhat.say("You are asking what I did at the night of the murder? I was at home. ")
-        furhat.gesture(Gestures.ExpressDisgust, async = true)
-        furhat.gesture(Gestures.BrowFrown(strength = 1.00))
-        furhat.gesture(rollEyes)
-        furhat.say("I went to bet around 11. I was a close friend of Albert I would never hurt him. Trust me.")
+        furhat.say("A4")
+
+        furhat.say("Hey I am so glad to see you detective!")
+        furhat.gesture(Gestures.ExpressDisgust(strength = 0.5, duration = 2.50), async = true)
+        furhat.gesture(Gestures.BrowFrown(strength = 0.30, duration = 2.50), async = true)
+        furhat.say("You are asking what I did at the night of the murder? I was at home. I went to bed around 11. If I knew Albert?")
+        furhat.gesture(rollEyes, async=true)
+        furhat.say("Yeah, yeah I knew him.")
+        furhat.gesture(Gestures.ExpressDisgust(duration = 2.00), async = true)
+        furhat.gesture(Gestures.BrowFrown(strength = 0.60, duration = 2.00), async = true)
+        furhat.say(" I was a close friend of Albert. I would never hurt him. Trust me.")
+        furhat.gesture(resetFace)
         delay(4000) // TODO: Change to listens for "yes" bf continuing
         goto(A5)
     }
@@ -275,9 +292,11 @@ val A4 = state(InteractionTest){
 val A5 = state(InteractionTest){
     // 1e Babbles a lot, and uses phrases such as hmm, errr.
     onEntry {
-        furhat.say("Yes sorry what's that? You are asking what I did at the night of the murder? yes yes I can answer that.  mmm I was at home. " +
-                "eee I went to bed around eeee 11 I think, eee yeah 11 Eee I was his friend. I was like really close, yes a" +
-                " close friend of him, Albert that is, I would never hurt him. Never hurt Albert. I wouldn't! Trust me.")
+        furhat.say("A5")
+
+        furhat.say("Hey I'm so glad to see you detective. Yes sorry what's that? You are asking what I did at the night of the murder? yes yes I can answer that. I was at home. " +
+                "And then I went to bed around 11 I think yes it must have been 11. I would never hurt him. I was his friend. We were really close, yes. " +
+                "I was a close friend of his, I would never hurt him. Never hurt Albert. I wouldn't! Trust me.")
         delay(4000) // TODO: Change to listens for "yes" bf continuing
         goto(A6)
     }
@@ -292,12 +311,15 @@ val A5 = state(InteractionTest){
 val A6 = state(InteractionTest){
     // 1f. Strong emotional expressivity, that correlates to what they are saying.
     onEntry {
-        furhat.gesture(Gestures.Smile(strength = 0.8), async = true)
+        furhat.say("A6")
+
+        furhat.gesture(Gestures.BigSmile(strength = 2.00), async = true)
         furhat.say("Hey I am so glad to see you detective!")
-        furhat.gesture(Gestures.ExpressSad(strength=5.00, duration=10.00), async = true)
-        furhat.say("You are asking what I did at the night of the murder? I was at home. " +
-                "I went to bet around 11. I was a close friend of Albert, I would never hurt him.")
-        furhat.gesture(Gestures.BrowFrown(strength = 3.00), async = true)
+        furhat.gesture(Gestures.ExpressFear(strength = 0.40), async = true)
+        furhat.say("You are asking what I did at the night of the murder?")
+        furhat.gesture(Gestures.Thoughtful(duration = 2.00), async = true)
+        furhat.say("I was at home. I went to bed around 11. I was a close friend of Albert, I would never hurt him.")
+        furhat.gesture(Gestures.BrowFrown(strength = 2.00), async = true)
         furhat.say(" Trust me.")
         delay(4000) // TODO: Change to listens for "yes" bf continuing
         goto(A7)
@@ -311,11 +333,13 @@ val A6 = state(InteractionTest){
 val A7 = state(InteractionTest){
     // 1g. Genuine smiles.
     onEntry {
+        furhat.say("A7")
+
         furhat.gesture(Gestures.BigSmile(strength = 2.0, duration = 2.0), async = true)
-        furhat.say("Hey! Detective! I am so glad to see you! It's been a crazy morning ")
+        furhat.say("Hey I am so glad to see you detective!")
         furhat.gesture(Gestures.Thoughtful(duration = 10.00))
         furhat.say("You are asking what I did at the night of the murder? I was at home. " +
-                "I went to bet around 11. I was a close friend of Albert, I would never hurt him.")
+                "I went to bed around 11. I was a close friend of Albert, I would never hurt him.")
         furhat.gesture(Gestures.ExpressSad, async = true)
         furhat.say("Trust me.")
         delay(4000) // TODO: Change to listens for "yes" bf continuing
@@ -330,9 +354,14 @@ val A7 = state(InteractionTest){
 val A8 = state(InteractionTest){
     // 1h. A lot of nodding and head shaking when listening.
     onEntry {
+        furhat.say("A8")
+
+        furhat.say("Hey I am so glad to see you detective!")
         furhat.gesture(Gestures.Nod(strength = 0.4, duration=2.00), async = true)
-        furhat.say("You are asking what I did at the night of the murder? Ok yes I can answer that. I was at home. " +
-                "I went to bet around 11. I was a close friend of Albert, I would never hurt him. Trust me.")
+        furhat.say("You are asking what I did at the night of the murder?")
+        furhat.gesture(Gestures.Nod(strength = 0.4, duration=2.00), async = true)
+        furhat.say("Ok yes I can answer that. I was at home. " +
+                "I went to bed around 11. I was a close friend of Albert, I would never hurt him. Trust me.")
         delay(4000) // TODO: Change to listens for "yes" bf continuing
         goto(A9)
     }
@@ -345,9 +374,12 @@ val A8 = state(InteractionTest){
 val A9 = state(InteractionTest){
     // 1i. Fewer head gestures (no glancing or gazing) shows attention and närvaro
     onEntry {
+        furhat.say("A9")
+
         furhat.gesture(Gestures.BrowRaise(duration = 10.00))
+        furhat.say("Hey I am so glad to see you detective!")
         furhat.say("You are asking what I did at the night of the murder? I was at home. " +
-                "I went to bet around 11. I was a close friend of Albert, I would never hurt him. Trust me.")
+                "I went to bed around 11. I was a close friend of Albert, I would never hurt him. Trust me.")
         delay(4000) // TODO: Change to listens for "yes" bf continuing
         goto(A10)
     }
@@ -357,29 +389,15 @@ val A9 = state(InteractionTest){
     }
 }
 
+
 val A10 = state(InteractionTest){
-    // 1j. Furhat does not pay any attention
+    // 1k. Neutral face.
     onEntry {
-        furhat.gesture(Gestures.BigSmile(strength = 2.0, duration = 10.0), async = true)
-        furhat.gesture(Gestures.GazeAway, async = true)
-        furhat.say("You are asking what I did at the night of the murder? I was at home. " +
-                "I went to bet around 11. I was a close friend of Albert, I would never hurt him. Trust me.")
-        delay(4000) // TODO: Change to listens for "yes" bf continuing
-        goto(A11)
-    }
+        furhat.say("A11")
 
-    onResponse<Yes> {
-        // goto(A2) TODO: add
-    }
-}
-
-val A11 = state(InteractionTest){
-    // 1k. . Neutral face.
-    onEntry {
-        furhat.gesture(Gestures.BigSmile(strength = 2.0, duration = 2.0), async = true)
-        furhat.gesture(Gestures.GazeAway, async = true)
+        furhat.say("Hey I am so glad to see you detective!")
         furhat.say("You are asking what I did at the night of the murder? I was at home. " +
-                "I went to bet around 11. I was a close friend of Albert, I would never hurt him. Trust me.")
+                "I went to bed around 11. I was a close friend of Albert, I would never hurt him. Trust me.")
         delay(100) // TODO: Change to listens for "yes" bf continuing
         goto(IdleTest)
 
